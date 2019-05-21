e2b3ddd227cb85961ee3aecf2cde80e0d577ef08
avi <anatoly.ivanov@gmail.com>
2019-05-20 21:59:22 -0700

    refactor credentials getting method

--- tesla.py
+++ tesla.py
@@ -10,7 +10,7 @@ class Tesla:
     root = 'https://owner-api.teslamotors.com/api/1/{}'    
 
     def __init__(self, username,password):
-      
+        ''' authenticate and save access token'''
         response = post('https://owner-api.teslamotors.com/oauth/token', data={
             'client_id': self.client_id,
             'client_secret': self.client_secret,
@@ -28,10 +28,12 @@ class Tesla:
         print('Received token: ...{}.'.format(self.token[:5]))
 
     def __del__(self):
+        ''' revoke access token to avoid pollution'''
         response = post('https://owner-api.teslamotors.com/oauth/revoke', data='token={}'.format(self.token))
         print('Token {} revoked with status code {}.'.format(self.token[:5], response.status_code))
 
     def get_json(self,method):
+        ''' execute an authenticated GET request against a given method'''
         headers = default_headers()
         headers.update({'Authorization': 'bearer {}'.format(self.token)}) 
     
@@ -42,6 +44,7 @@ class Tesla:
         return json
     
     def post_json(self,method,json_data):
+        ''' execute an authenticated POST request against a given method, post provided data'''
         headers = default_headers()
         headers.update({'Authorization': 'bearer {}'.format(self.token)}) 
     
@@ -51,17 +54,19 @@ class Tesla:
     
         return json
 
+def get_credentials(default_email):
+    login = input('Enter your tesla.com login: [default={}]: '.format(default_email))
+    if not login: 
+        login = default_email
+    else:
+        login = login.strip()
+
+    password = getpass('Please eneter password for {}: '.format(login))
+    return login,password
 
 def main():
 
-    default_email = "anatoly.ivanov@gmail.com" 
-    login = input('Enter your tesla.com login: [default={}]: '.format(default_email))
-    if not login: 
-        login=default_email
-    else:
-        login=login.strip()
-
-    password = getpass('Please eneter password for {} :'.format(login))
+    (login,password) = get_credentials(default_email="anatoly.ivanov@gmail.com")
 
     tesla = Tesla(login, password)
 
