18b67691bb9d443330038c6a9d030c7688c9fd5c
avi <anatoly.ivanov@gmail.com>
2019-05-20 21:56:36 -0700

    refactor into a class

--- tesla.py
+++ tesla.py
@@ -3,67 +3,71 @@ from requests.utils import default_headers
 from getpass import getpass
 from json import dumps  # https://docs.python.org/2/library/json.html
     
-## config
-client_id="81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
-client_secret="c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
-root = 'https://owner-api.teslamotors.com/api/1/{}'    
-email = "anatoly.ivanov@gmail.com" 
+class Tesla:
+    ## config
+    client_id="81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
+    client_secret="c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
+    root = 'https://owner-api.teslamotors.com/api/1/{}'    
 
-def get_token(username,password):
-    ''' get an access token in exchange for login+password '''
+    def __init__(self, username,password):
+      
+        response = post('https://owner-api.teslamotors.com/oauth/token', data={
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'password',
+            'email': username,
+            'password': password,
+            })
+
+        print('Received {} {} response.'.format(response.status_code, response.reason))
+        if response.status_code > 299: 
+            raise Exception('Invalid response')
+
+        payload = response.json()
+        self.token = payload['access_token']
+        print('Received token: ...{}.'.format(self.token[:5]))
+
+    def __del__(self):
+        response = post('https://owner-api.teslamotors.com/oauth/revoke', data='token={}'.format(self.token))
+        print('Token {} revoked with status code {}.'.format(self.token[:5], response.status_code))
+
+    def get_json(self,method):
+        headers = default_headers()
+        headers.update({'Authorization': 'bearer {}'.format(self.token)}) 
     
+        response = get(self.root.format(method),headers=headers)
+        json = response.json()
+        print(dumps(json, sort_keys=True, indent=4, separators=(',', ': ')))
     
-    response = post('https://owner-api.teslamotors.com/oauth/token', data={
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'password',
-        'email': username,
-        'password': password,
-        })
-
-    # DEBUG:
-    print('Received {} {} response.'.format(response.status_code, response.reason))
-    if response.status_code > 299: raise Exception('Invalid response')
-
-    payload = response.json()
-    token= payload['access_token']
-    print('Received token: ...{}.'.format(token[:5]))
-    return token
-
-def revoke_token(token):
-    response = post('https://owner-api.teslamotors.com/oauth/revoke', data='token={}'.format(token))
-    print('Token {} revoked with status code {}.'.format(token[:5], response.status_code))
-
-def get_authenticated_json(method,access_token):
-    headers = default_headers()
-    headers.update({'Authorization': 'bearer {}'.format(access_token)}) 
+        return json
     
-    response = get(root.format(method),headers=headers)
-    json = response.json()
-    print(dumps(json, sort_keys=True, indent=4, separators=(',', ': ')))
+    def post_json(self,method,json_data):
+        headers = default_headers()
+        headers.update({'Authorization': 'bearer {}'.format(self.token)}) 
     
-    return json
-
+        response = post(self.root.format(method),data=json_data,headers=headers)
+        json = response.json()
+        print(dumps(json, sort_keys=True, indent=4, separators=(',', ': ')))
+    
+        return json
 
 
 def main():
 
-
-    login = input('Enter your tesla.com login: [default={}]: '.format(email))
+    default_email = "anatoly.ivanov@gmail.com" 
+    login = input('Enter your tesla.com login: [default={}]: '.format(default_email))
     if not login: 
-        login=email
+        login=default_email
     else:
         login=login.strip()
 
     password = getpass('Please eneter password for {} :'.format(login))
- 
-    access_token = get_token(login, password)
-    try:
-        vehicle_id = get_authenticated_json('/vehicles',access_token)['response'][0]['id']    
-        print('Working with vehicle_id={}'.format(vehicle_id))
-        data = get_authenticated_json('/vehicles/{}/data'.format(vehicle_id),access_token)
-    finally:
-        revoke_token(access_token)
 
+    tesla = Tesla(login, password)
+
+    vehicle_id = tesla.get_json('/vehicles')['response'][0]['id']    
+    print('Working with vehicle_id={}'.format(vehicle_id))
+
+    data = tesla.get_json('/vehicles/{}/data'.format(vehicle_id))
 
 main()
