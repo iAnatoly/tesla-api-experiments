852ce3b51d04daebd890eaf237bbc2886a674a69
iAnatoly <anatoly.ivanov@gmail.com>
2019-05-23 19:06:11 -0700

    set charge limit command

--- tesla.py
+++ tesla.py
@@ -2,8 +2,9 @@ from requests import post, get
 from requests.utils import default_headers
 from getpass import getpass
 from json import dumps  # https://docs.python.org/2/library/json.html
+from argparse import ArgumentParser # https://docs.python.org/3.3/library/argparse.html
 
-DEBUG = 0
+DEBUG = 1
 
 class Tesla:
     ## config
@@ -23,6 +24,7 @@ class Tesla:
 
         if (DEBUG): print('Received {} {} response.'.format(response.status_code, response.reason))
         if response.status_code > 299: 
+            self.token = None
             raise Exception('Invalid response')
 
         payload = response.json()
@@ -31,8 +33,9 @@ class Tesla:
 
     def __del__(self):
         ''' revoke access token to avoid pollution'''
-        response = post('https://owner-api.teslamotors.com/oauth/revoke', data='token={}'.format(self.token))
-        print('Token {} revoked with status code {}.'.format(self.token[:5], response.status_code))
+        if self.token:
+            response = post('https://owner-api.teslamotors.com/oauth/revoke', data='token={}'.format(self.token))
+            print('Token {} revoked with status code {}.'.format(self.token[:5], response.status_code))
 
     def get_json(self,method):
         ''' execute an authenticated GET request against a given method'''
@@ -53,7 +56,9 @@ class Tesla:
         response = post(self.root.format(method),data=json_data,headers=headers)
         json = response.json()
         if (DEBUG): print(dumps(json, sort_keys=True, indent=4, separators=(',', ': ')))
-    
+        if response.status_code > 299: 
+            raise Exception('Invalid response')
+
         return json
 
 def get_credentials(default_email):
@@ -66,17 +71,7 @@ def get_credentials(default_email):
     password = getpass('Please eneter password for {}: '.format(login))
     return login,password
 
-def main():
-
-    (login,password) = get_credentials(default_email="anatoly.ivanov@gmail.com")
-
-    tesla = Tesla(login, password)
-
-    vehicle_id = tesla.get_json('/vehicles')['response'][0]['id']    
-    print('Working with vehicle_id={}'.format(vehicle_id))
-
-    data = tesla.get_json('/vehicles/{}/data'.format(vehicle_id))
-
+def print_stats(data):
     firmware_version = data['response']['vehicle_state']['car_version']
     print('Firmware version: {}'.format(firmware_version))
 
@@ -93,12 +88,38 @@ def main():
     
     wh_added = charge['charge_energy_added']
     mi_added = charge['charge_miles_added_ideal']
-
+    
     print('Charge started at {}'.format(battery_range-mi_added))
+
+    charge_limit = charge['charge_limit_soc']
+    print('Charge limit: {}'.format(charge_limit))
 
     rate = 0.24 # let us use supercharger rate as a baseline
 
-    print('Engergy added: {} ({} miles). That would be ${} at {} per wh'.format(wh_added, mi_added, wh_added*rate, rate))
+    print('Engergy added: {}kwh ({} miles). That would be ${} at {} per kwh'.format(wh_added, mi_added, wh_added*rate, rate))
     
+def get_charge_limit_from_commandline():
+    parser = ArgumentParser(description='Send commands to tesla.')
+    parser.add_argument('--set-charge-limit', dest='limit', type=int, default=0, help='Percentage of the battery to set the charge to')
+    args = parser.parse_args()
+    return args.limit
+
+def main():
+
+    (login,password) = get_credentials(default_email="anatoly.ivanov@gmail.com")
+
+    tesla = Tesla(login, password)
+
+    vehicle_id = tesla.get_json('/vehicles')['response'][0]['id']    
+    print('Working with vehicle_id={}'.format(vehicle_id))
+
+    data = tesla.get_json('/vehicles/{}/data'.format(vehicle_id))
+    print_stats(data)
+
+    limit = get_charge_limit_from_commandline()
+    if limit>0:
+        print('Updating the limit: {}->{}'.format(data['response']['charge_state']['charge_limit_soc'],limit))
+        tesla.post_json('/vehicles/{}/command/set_charge_limit'.format(vehicle_id), json_data={ 'percent': limit })
+           
 
 main()
