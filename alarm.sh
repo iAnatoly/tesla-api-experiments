104af821c3b97e32084ad9534092ca5b4d0b1cbf
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2019-10-16 18:12:56 -0700

    figure out the parent dir to run tesla.py

--- alarm.sh
+++ alarm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-STATE=`./tesla.py | grep "Charge state:" | cut -d ' ' -f 7`
+DIR=`dirname "$(readlink -f $0)"`
+STATE=`$DIR/tesla.py | grep "Charge state:" | cut -d ' ' -f 7`
 if [ "x${STATE}" != "xCharging" ] ; then
     amixer -D pulse sset Master 100%
     espeak "ALARM! ALARM! Tesla Not Charging"
