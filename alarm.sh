33218983c3a0397028b392e94c56243cd0aca22b
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2019-10-16 18:16:51 -0700

    turn on the speaker

--- alarm.sh
+++ alarm.sh
@@ -2,6 +2,7 @@
 DIR=`dirname "$(readlink -f $0)"`
 STATE=`$DIR/tesla.py | grep "Charge state:" | cut -d ' ' -f 7`
 if [ "x${STATE}" != "xCharging" ] ; then
+    amixer -D pulse sset Master on
     amixer -D pulse sset Master 100%
     espeak "ALARM! ALARM! Tesla Not Charging"
 fi
